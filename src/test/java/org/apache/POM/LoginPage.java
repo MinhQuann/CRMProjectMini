595c6c6916a01491fda8bf8d424a5b5d454f32e4
MinhQuann <buinguyenminhquan98@gmail.com>
2023-08-25 16:59:52 +0700

    fix pom3

--- src/test/java/org/apache/POM/LoginPage.java
+++ src/test/java/org/apache/POM/LoginPage.java
@@ -3,6 +3,7 @@ package org.apache.POM;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,6 +14,7 @@ import java.time.Duration;
 public class LoginPage extends PageObjectBase {
 
     WebDriver driver;
+
 
     By clickBtnLoginOutSidePage = By.cssSelector("button.ant-btn-primary");
 
@@ -49,7 +51,8 @@ public class LoginPage extends PageObjectBase {
         super(driver);
     }
 
-    public void Open(){
+    public void Open()
+    {
         this.driver.get("https://lab.connect247.vn/ucrm-sso/");
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         this.getDynamicElement(clickBtnLoginOutSidePage).click();
--- src/test/java/org/apache/TestCase/TestCase.java
+++ src/test/java/org/apache/TestCase/TestCase.java
@@ -2,6 +2,7 @@ package org.apache.TestCase;
 
 import org.apache.DashBoard.DashBoardCRM;
 import org.apache.POM.LoginPage;
+import org.apache.POM.PageObjectBase;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,6 +14,8 @@ public class TestCase {
     WebDriver driver;
     LoginPage loginPage;
     DashBoardCRM dashBoardCRM;
+
+    PageObjectBase pageObjectBase;
     @Before
     public void InitTest(){
         //PC nhà
@@ -23,13 +26,14 @@ public class TestCase {
         this.driver = new ChromeDriver();
         this.dashBoardCRM = new DashBoardCRM(this.driver);
         this.loginPage = new LoginPage(this.driver);
+        this.pageObjectBase = new PageObjectBase(this.driver);
 
     }
 
     @Test
     public void Verify_Login_with_Email_Pwd_Correct(){
-
-        this.loginPage.Open();
+        this.loginPage.Open(
+        );
         this.loginPage.Login("quanbnm@gmail.com","12345678x@X");
         try {
                 Thread.sleep(4000);
